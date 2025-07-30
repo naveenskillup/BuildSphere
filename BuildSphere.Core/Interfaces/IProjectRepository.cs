65ea196e154f93c8bcf73be0d299046a72bd3749
naveenskillup <naveenb1195@gmail.com>
2025-07-30 21:07:28 +0530

    feat: added SharedFileStorageManager

---
+++ BuildSphere.Core/Interfaces/IProjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuildSphere.Core.Definitions;
+
+namespace BuildSphere.Core.Interfaces
+{
+    public interface IProjectRepository
+    {
+        Project GetById(int id);
+        IEnumerable<Project> Get();
+        void Add(Project project);
+        void Update(Project project);
+        void Delete(int id);
+    }
+}
---
+++ BuildSphere.Data/TextFile/SharedFileStorageManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BuildSphere.Core.Definitions;
+
+namespace BuildSphere.Data.TextFile
+{
+    public abstract class SharedFileStorageManager<TObject>
+    {
+        public SharedFileStorageManager(string filePath) 
+        {
+            _filePath = filePath;
+        }
+
+        private void Load()
+        {
+            if (!File.Exists(_filePath)) { }
+                return;
+
+            var jsonString = File.ReadAllText(_filePath);
+            _data = JsonSerializer.Deserialize<List<TObject>>(jsonString);
+        }
+
+        private void Save()
+        {
+            lock (_lock)
+            {
+                var json = JsonSerializer.Serialize(_data, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(_filePath, json);
+            }
+        }
+
+        private List<TObject> _data = new();
+        private readonly object _lock = new();
+        private readonly string _filePath;
+    }
+}
