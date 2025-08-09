b75ef3f09ff201c19207cf594186ee0e3e13f923
naveenskillup <naveenb1195@gmail.com>
2025-08-09 20:59:54 +0530

    feat: ui project scaffold

---
+++ BuildSphere.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BuildSphere.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
