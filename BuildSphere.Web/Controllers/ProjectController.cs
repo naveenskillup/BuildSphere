45b0df4d516fcdf2fffbf7e91c15941271c3a5e0
naveenskillup <naveenb1195@gmail.com>
2025-08-21 08:55:39 +0530

    feat: add project ui page

---
+++ BuildSphere.Web/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuildSphere.Web.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
